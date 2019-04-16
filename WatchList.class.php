e74251e0b9aa0fecb7f5d35357eebebf8958f1e1
Pwirth <wirth@hallowelt.biz>
2019-04-16 07:30:05 +0000

    Fix license and version in extension doc
    Change-Id: I65bc1842e2b77e26d8e27043bd8e52d96a52457d

--- WatchList.class.php
+++ WatchList.class.php
@@ -6,8 +6,7 @@
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * the Free Software Foundation, version 3.
  *
  * This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -22,11 +21,11 @@
  * For further information visit http://www.bluespice.com
  *
  * @author     Robert Vogel <vogel@hallowelt.com>
- * @version    2.23.1
+ * @version    3.0.0
  * @package    BlueSpice_Extensions
  * @subpackage WatchList
  * @copyright  Copyright (C) 2016 Hallo Welt! GmbH, All rights reserved.
- * @license    http://www.gnu.org/copyleft/gpl.html GPL-2.0-or-later
+ * @license    http://www.gnu.org/copyleft/gpl.html GPL-3.0-only
  * @filesource
  */
 
