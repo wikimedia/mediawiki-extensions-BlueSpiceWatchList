17a65fab06eff08899009bdd6812fe1539795135
pwirth <wirth@hallowelt.biz>
2020-01-20 22:04:03 +0100

    Use new hook handler base class for BSInsertMagicAjaxGetData
    ERM:10378
    [3.2]
    
    Change-Id: Ida053d515640b74f16c6149e288a096699144662

---
+++ src/Hook/BSInsertMagicAjaxGetData/AddWatchListTag.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace BlueSpice\WatchList\Hook\BSInsertMagicAjaxGetData;
+
+use BlueSpice\InsertMagic\Hook\BSInsertMagicAjaxGetData;
+
+class AddWatchListTag extends BSInsertMagicAjaxGetData {
+
+	/**
+	 *
+	 * @return bool
+	 */
+	protected function skipProcessing() {
+		return $this->type !== 'tags';
+	}
+
+	/**
+	 *
+	 * @return bool
+	 */
+	protected function doProcess() {
+		$this->response->result[] = (object)[
+			'id' => 'bs:watchlist',
+			'type' => 'tag',
+			'name' => 'watchlist',
+			'desc' => $this->msg( 'bs-watchlist-tag-watchlist-desc' )->text(),
+			'code' => '<bs:watchlist />',
+			'previewable' => false,
+			'helplink' => $this->getHelpLink()
+		];
+
+		return true;
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	private function getHelpLink() {
+		return $this->getServices()->getBSExtensionFactory()
+			->getExtension( 'BlueSpiceWatchList' )->getUrl();
+	}
+
+}
