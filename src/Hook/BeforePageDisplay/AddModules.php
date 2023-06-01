f224429fe95f4ef21076e39c9f8125f2fb8e1cf2
miriam schlindwein <schlindwein@hallowelt.com>
2023-06-01 12:21:06 +0200

    Remove unused style module
    ERM31248
    
    [4.3.x]
    
    Change-Id: I44af087e18f8df71afe1a7051e6911e8010ec0b9

--- src/Hook/BeforePageDisplay/AddModules.php
+++ src/Hook/BeforePageDisplay/AddModules.php
@@ -7,7 +7,6 @@ use BlueSpice\Hook\BeforePageDisplay;
 class AddModules extends BeforePageDisplay {
 
 	protected function doProcess() {
-		$this->out->addModuleStyles( 'ext.bluespice.watchlist.styles' );
 		$this->out->addModules( 'ext.bluespice.watchlist.myWatchlistWindow.bootstrap' );
 
 		return true;
