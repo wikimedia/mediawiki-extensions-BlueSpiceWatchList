8b75407a1282a87e6e0dd72a2fec771292ac7626
hslater <hamish.slater@hallowelt.com>
2025-02-07 15:21:09 +0100

    Update PPFrame namespace
    ERM38444
    
    [REL1_43, master]

--- src/Tag/WatchList.php
+++ src/Tag/WatchList.php
@@ -9,7 +9,7 @@ use BlueSpice\Tag\MarkerType;
 use BlueSpice\Tag\MarkerType\NoWiki;
 use BlueSpice\Tag\Tag;
 use MediaWiki\Parser\Parser;
-use PPFrame;
+use MediaWiki\Parser\PPFrame;
 use ValueValidators\RangeValidator;
 use ValueValidators\StringValidator;
 
