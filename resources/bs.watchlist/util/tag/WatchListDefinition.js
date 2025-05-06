945b92dee3e5113a27810819b036462d2021b6b8
hslater <hamish.slater@hallowelt.com>
2025-05-06 16:44:34 +1200

    eslint
    ERM20742
    
    [REL1_43, REL1_43-5.0.x, master]
    
    Change-Id: If157ff1b4f48fa09977a46698a4a35878fb9935d

--- resources/bs.watchlist/util/tag/WatchListDefinition.js
+++ resources/bs.watchlist/util/tag/WatchListDefinition.js
@@ -5,27 +5,27 @@ bs.watchlist.util.tag.WatchListDefinition = function BsVecUtilTagWatchListDefini
 
 OO.inheritClass( bs.watchlist.util.tag.WatchListDefinition, bs.vec.util.tag.Definition );
 
-bs.watchlist.util.tag.WatchListDefinition.prototype.getCfg = function() {
-	var cfg = bs.watchlist.util.tag.WatchListDefinition.super.prototype.getCfg.call( this );
-	return $.extend( cfg, {
-		classname : 'Watchlist',
+bs.watchlist.util.tag.WatchListDefinition.prototype.getCfg = function () {
+	const cfg = bs.watchlist.util.tag.WatchListDefinition.super.prototype.getCfg.call( this );
+	return $.extend( cfg, { // eslint-disable-line no-jquery/no-extend
+		classname: 'Watchlist',
 		name: 'watchlist',
 		tagname: 'bs:watchlist',
 		descriptionMsg: 'bs-watchlist-tag-watchlist-desc',
 		menuItemMsg: 'bs-watchlist-ve-watchlistinspector-title',
-		attributes: [{
+		attributes: [ {
 			name: 'count',
 			labelMsg: 'bs-watchlist-ve-watchlist-attr-count-label',
 			helpMsg: 'bs-watchlist-tag-watchlist-attr-count-help',
 			type: 'number',
 			default: 5
-		},{
+		}, {
 			name: 'maxtitlelength',
 			labelMsg: 'bs-watchlist-ve-watchlist-attr-maxtitlelength-label',
 			helpMsg: 'bs-watchlist-tag-watchlist-attr-maxtitlelength-help',
 			type: 'number',
 			default: 20
-		},{
+		}, {
 			name: 'order',
 			labelMsg: 'bs-watchlist-ve-watchlist-attr-order-label',
 			helpMsg: 'bs-watchlist-tag-watchlist-attr-order-help',
@@ -35,13 +35,13 @@ bs.watchlist.util.tag.WatchListDefinition.prototype.getCfg = function() {
 				{
 					data: 'pagename',
 					label: mw.message( 'bs-watchlist-ve-watchlist-attr-pagename-label' ).text()
-				},{
+				}, {
 					data: 'time',
 					label: mw.message( 'bs-watchlist-ve-watchlist-attr-time-label' ).text()
 				}
 			]
-		},]
-	});
+		} ]
+	} );
 };
 
 bs.vec.registerTagDefinition(
