39874517f811ed2ecc46530e9a54fa7ac94335ff
Hamish <hamish.slater@hallowelt.com>
2026-07-15 17:56:44 +0700

    phpcs

--- src/Extension.php
+++ src/Extension.php
@@ -1,41 +1,8 @@
 <?php
-/**
- * WatchList extension for BlueSpice
- *
- * Adds the watchlist to focus.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 3.
- *
- * This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- *
- * This file is part of BlueSpice MediaWiki
- * For further information visit https://bluespice.com
- *
- * @author     Robert Vogel <vogel@hallowelt.com>
- * @version    3.0.0
- * @package    BlueSpice_Extensions
- * @subpackage WatchList
- * @copyright  Copyright (C) 2016 Hallo Welt! GmbH, All rights reserved.
- * @license    http://www.gnu.org/copyleft/gpl.html GPL-3.0-only
- * @filesource
- */
+
 namespace BlueSpice\WatchList;
 
 use BlueSpice\Extension as BaseExtension;
 
-/**
- * Base class for WatchList extension
- * @package BlueSpice_Extensions
- * @subpackage WantedArticle
- */
 class Extension extends BaseExtension {
 }
